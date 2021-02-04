251d16eb91d1be6b6a5a64482185999416867883
Gerry Koh <kreator303@gmail.com>
2021-02-04 16:50:50 -0700

    initial commit

---
+++ source/scripts/email_list.js
@@ -0,0 +1,9 @@
+function ValidateEmail(mail) 
+{
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mc-embedded-subscribe-form.mce-EMAIL.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
